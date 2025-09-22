97b2d50c1da7636c1fefc378b947db73ad4ca1ef
mhpasp.ads.etec <mhpasp.ads.etec@gmail.com>
2025-09-22 01:51:05 -0300

    não rodou ate aqui

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,24 +1,55 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Services;
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+
+        private async Task Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            try
+            {
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
+                {
+                    Tempo? t = await DataService.GetPrevisao(txt_cidade.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                    if (t != null)
+                    {
+                        string dados_previsao = "";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+                        dados_previsao = $"Latitude: {t.lat} \n" +
+                            $"Longitude: {t.lon} \n" +
+                            $"Nascer do sol: {t.sunrise} \n" +
+                            $"Por do sol: {t.sunset} \n" +
+                            $"Temperatura Maxima: {t.temp_max} \n" +
+                            $"Temperatura Minima: {t.temp_min} \n";
+
+                    }
+                    else
+                    {
+                        lbl_res.Text = "Sem dados de previsão.";
+                    }
+
+                }
+                else
+                {
+                    lbl_res.Text = "Preencha a Cidade.";
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Ops", ex.Message, "OK");
+
+            }
         }
     }
 
